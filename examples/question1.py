d79da9a644e9bb77cb933bbafcce42fc326aec8c
Janis <janis@jaggedverge.com>
2018-07-20 16:37:54 +1000

    Robbie's first question

---
+++ examples/question1.py
@@ -0,0 +1,7 @@
+""" Robbie's first question, what does mypy output with this code?"""
+class Item:
+   def __init__(self, price: float):
+       self.price = price
+
+item = Item(price=1.5)
+item.price + "abc"
