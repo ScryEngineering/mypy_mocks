c37d10cd39c3885b328f3f2cfa7d226280544b82
Janis <janis@jaggedverge.com>
2018-07-20 18:19:57 +1000

    Start kwargs dispatch example

--- examples/annotating_kwargs.py
+++ examples/annotating_kwargs.py
@@ -3,9 +3,38 @@
 
 # If all the kwargs to outer are floats we can do the following
 def outer(**kwargs: float):
-   inner(**kwargs)
+    inner(**kwargs)
 
 def inner(num: float):
-   print(num * 2)
+    print(num * 2)
 
 outer(num="abc")
+
+
+
+# If however we are using kwargs to dispatch on types we have to do something different
+from typing import List
+
+def outer2(**kwargs):
+    if isinstance(kwargs['num'], list):
+        inner_lists(**kwargs)
+    else:
+        inner_floats(**kwargs)
+
+def inner_lists(num: List[float]):
+    for item in num:
+        print(num * 2)
+
+def inner_floats(num: float):
+    print(num * 2)
+
+
+# OK
+outer2(num=0.1)
+
+# OK
+outer2(num=[0.2,0.3])
+
+# bad
+outer2(num="abc")
+
