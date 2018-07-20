66bef8b39a8ad5f327dab34c109daba9e767ce02
Janis <janis@jaggedverge.com>
2018-07-20 16:39:11 +1000

    Robbie's second mypy question

---
+++ examples/question2.py
@@ -0,0 +1,8 @@
+""" Robbie's second question, what does mypy output with this function forwarding code?"""
+def outer(**kwargs):
+   inner(**kwargs)
+
+def inner(num: float):
+   print(num * 2)
+
+outer(num="abc")
